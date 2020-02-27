59db83b21bdb6cefaf3753576f7b4351bdce65bd
Harold Mendoza <haroldrmendoza@MacBook-Pro-de-Harold.local>
2020-02-27 14:46:06 -0600

    Profiles Finished

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -40,7 +40,7 @@ const Profile = ({
             <ProfileTop profile={profile} />
             <ProfileAbout profile={profile} />
             <div className="profile-exp bg-white p-2">
-              <h2 className="text-primary">Experience</h2>
+              <h2 className="text-primary">Experience 🔥</h2>
               {profile.experience.length > 0 ? (
                 <Fragment>
                   {profile.experience.map(experience => (
@@ -56,7 +56,7 @@ const Profile = ({
             </div>
 
             <div className="profile-edu bg-white p-2">
-              <h2 className="text-primary">Education</h2>
+              <h2 className="text-primary">Education 🎓</h2>
               {profile.education.length > 0 ? (
                 <Fragment>
                   {profile.education.map(education => (
