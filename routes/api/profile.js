01d9d3d227f350bb8a0f1d64b8947601d073e61d
Harold Mendoza <haroldrmendoza@MacBook-Pro-de-Harold.local>
2020-02-26 22:40:22 -0600

    Bug Fixed, Profiles Work

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -130,12 +130,12 @@ router.get("/", async (req, res) => {
 
 router.get("/user/:user_id", async (req, res) => {
   try {
-    const profiles = await Profile.findOne({
+    const profile = await Profile.findOne({
       user: req.params.user_id
     }).populate("user", ["name", "avatar"]);
     if (!profile)
       return res.status(400).json({ msg: "There is no profile for this user" });
-    res.json(profiles);
+    res.json(profile);
   } catch (err) {
     console.error(err.message);
     if (err.kind == "ObjectId") {
